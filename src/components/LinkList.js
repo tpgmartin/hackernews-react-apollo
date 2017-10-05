f94cade71c6455117f304f07d9a7b1910f4bb093
Tom Martin <tom@tpgmartin.com>
2017-10-05 14:39:45 +0100

    Make graphql query to graph cool

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
+import { graphql, gql } from 'react-apollo'
 import Link from './Link'
 
 class LinkList extends Component {
 
   render() {
 
-    const linksToRender = [{
-      id: '1',
-      description: 'The Coolest GraphQL Backend 😎',
-      url: 'https://www.graph.cool'
-    }, {
-      id: '2',
-      description: 'The Best GraphQL Client',
-      url: 'http://dev.apollodata.com/'
-    }]
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading</div>
+    }
+
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>
+    }
+
+    const linksToRender = this.props.allLinksQuery.allLinks
 
     return (
       <div>
@@ -26,4 +27,21 @@ class LinkList extends Component {
 
 }
 
-export default LinkList
+// Create constant to store query
+// Operation name is `AllLinksQuery`
+// gql parses the plain GraphQL code
+const ALL_LINKS_QUERY = gql`
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+// Use graphql container to combine the LinkList component with the ALL_LINKS_QUERY
+// `allLinksQuery` is the name of the prop that Apollo injects into the `LinkList` component
+// (by default the prop would be called data)
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
